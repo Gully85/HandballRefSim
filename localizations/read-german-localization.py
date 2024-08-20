374dda74f480ca143753bb507c553517c0cb5f27
Malte Lütje <Malte.Luetje@posteo.de>
2024-08-20 16:10:12 +0200

    add empty read-script

---
+++ localizations/read-german-localization.py
@@ -0,0 +1,11 @@
+# Python script file to read the german localization of the rule-questions
+# This requires the input "Fragenkatalog" pdf file as parameter (or uses default in the main folder)
+# This will create or update localizations/german.csv
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
